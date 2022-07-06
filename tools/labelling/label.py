2ccc8697e5da4d7b7e8eeb6438a9ae3f00b956a9
Tom <tom.mladenov@hotmail.com>
2022-07-07 00:45:26 +0200

    Write labelling output to csv file

--- tools/labelling/label.py
+++ tools/labelling/label.py
@@ -6,6 +6,7 @@ import argparse
 from sys import byteorder
 import numpy as np
 import sys
+from os.path import exists
 
 from PyQt5 import QtWidgets, QtCore, uic, QtGui
 from pyqtgraph import PlotWidget, plot
@@ -13,7 +14,11 @@ import pyqtgraph as pg
 import glob, os
 import subprocess
 import imageio
+import csv
+import logging
 
+ROI_HEIGHT_BEACON_LONG = 77
+ROI_HEIGHT_BEACON_SHORT = 66
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -21,20 +26,29 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         uic.loadUi('/tools/labelling/gui.ui', self)
-        self.current_index = 0
-        self.data = []
-        self.selected_beacons = []
+        self.first = True
 
-        #self.imv = pg.ImageView()
-        print('ok')
+        # list with CSV lines
+        self.csv_content = []
+
+        # current file index
+        self.current_index = 0
+        self.current_filename = ""
+
+        # holds numpy data for one image at a time
+        self.data = []
+
+        # list of currently highlighed beacons, contains qgraphicsrectitems
+        self.selected_beacons_per_frame = []
+
+        self.beacon_graphics = []
 
         self.p1 = self.win.addPlot(title="")
         self.img = pg.ImageItem(axisOrder='row-major')
         self.img.hoverEvent = self.imageHoverEvent
-        #self.img.mouseClickEvent = self.mouseClickEvent
         self.p1.addItem(self.img)
 
-        self.selector_roi = pg.ROI([0, 0], [22, 66], pen='r')
+        self.selector_roi = pg.ROI([0, 0], [22, ROI_HEIGHT_BEACON_SHORT], pen='r')
         self.selector_roi.setAcceptedMouseButtons(QtCore.Qt.MouseButton.LeftButton)
         self.selector_roi.sigRegionChanged.connect(self.update_zoom)
         self.selector_roi.sigClicked.connect(self.tag_beacon)
@@ -48,12 +62,12 @@ class MainWindow(QtWidgets.QMainWindow):
     # function triggered by pressing B key, toggles for a long beacon or short beacon selection
     def toggle_beacon_length(self):
         w, h = self.selector_roi.size()
-        if h == 66:
+        if h == ROI_HEIGHT_BEACON_SHORT:
             #current ROI selector is for a short beacon, make it a long one
-            self.selector_roi.setSize((22, 77))
+            self.selector_roi.setSize((22, ROI_HEIGHT_BEACON_LONG))
         else:
             #current selector ROI is for a long beacon, make it a short one
-            self.selector_roi.setSize((22, 66))
+            self.selector_roi.setSize((22, ROI_HEIGHT_BEACON_SHORT))
 
     # function that updates the zoomed in view of currently selected beacon when mouse moves
     def update_zoom(self, roi):
@@ -61,6 +75,9 @@ class MainWindow(QtWidgets.QMainWindow):
  
     def set_file_list(self, file_list):
         self.file_list = file_list
+
+    def set_output_file(self, output_file):
+        self.output_file = output_file
 
     # process key events for app control
     def keyPressEvent (self, eventQKeyEvent):
@@ -77,18 +94,26 @@ class MainWindow(QtWidgets.QMainWindow):
 
     # clears all selected beacons, triggerd by pressing R key (RESET)
     def reset(self):
-        for b in self.selected_beacons:
-            self.p1.removeItem(b) 
+        for b in self.beacon_graphics:
+            self.p1.removeItem(b)
+
+        self.beacon_graphics = []
+        self.selected_beacons_per_frame = []
 
     # triggered when clicking, will add a rectangle where the ROI is currently located
     def tag_beacon(self, event):
         x, y = self.selector_roi.pos()
         w, h = self.selector_roi.size()
-        r1 = pg.QtGui.QGraphicsRectItem(x, y, w, h)
-        #r1.setPen(pg.mkPen(None))
-        r1.setPen(pg.mkPen('g'))
-        self.p1.addItem(r1)
-        self.selected_beacons.append(r1)
+        rectangle = pg.QtGui.QGraphicsRectItem(x, y, w, h)
+        rectangle.setPen(pg.mkPen('g'))
+        self.p1.addItem(rectangle)
+        beacon = {}
+        beacon['xmin'] = int(x)
+        beacon['ymin'] = int(y)
+        beacon['xmax'] = int(x + w)
+        beacon['ymax'] = int(y + h)
+        self.selected_beacons_per_frame.append(beacon)
+        self.beacon_graphics.append(rectangle)
 
 
 
@@ -105,33 +130,75 @@ class MainWindow(QtWidgets.QMainWindow):
         val = self.data[i, j]
         ppos = self.img.mapToParent(pos)
         x, y = ppos.x(), ppos.y()
-        self.p1.setTitle("pos: (%0.2f, %0.2f)  pixel: (%d, %d)" % (y, x, j, i))
+        self.p1.setTitle("pos: (%0.2f, %0.2f)  pixel: (%d, %d)" % (x, y, j, i))
         self.selector_roi.setPos(x - 11, y - 23)
+
+    def write_content(self):
+        with open(self.output_file, 'w') as f: 
+            write = csv.writer(f) 
+            write.writerow(['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax'])
+            write.writerows(self.csv_content)
+        print(self.csv_content)
+
+    def closeEvent(self, event):
+        self.write_content()
 
     # function to move to next raw file
     def next(self):
+
+        if not self.first:
+            
+            image_height, image_width, level = self.data.shape
+
+            # first check if the user tagged some beacons!
+            if len(self.selected_beacons_per_frame) == 0:
+                # no beacons :(
+                csv_line = [self.current_filename, str(image_width), str(image_height), "", "", "", "", ""]
+                self.csv_content.append(csv_line)
+            else:
+                # 1 or more beacons :)))
+                for beacon in self.selected_beacons_per_frame:
+                    if beacon['ymax'] - beacon['ymin'] == ROI_HEIGHT_BEACON_LONG:
+                        csv_line = [self.current_filename, str(image_width), str(image_height), "beacon_long", beacon['xmin'], beacon['ymin'], beacon['xmax'], beacon['ymax']]
+                    else:
+                        csv_line = [self.current_filename, str(image_width), str(image_height), "beacon_short", beacon['xmin'], beacon['ymin'], beacon['xmax'], beacon['ymax']]
+                    self.csv_content.append(csv_line)
+
+        if self.first:
+            self.first = False
+
+        self.reset()
+
         self.current_index += 1
         try:
-            print("laoding image: {}".format(self.file_list[self.current_index]))
+            print("loading image: {}".format(self.file_list[self.current_index]))
             self.show_image(self.file_list[self.current_index])
         except Exception as e:
             print(e)
 
     # function to move to previous raw file (will overwrite output)
     def previous(self):
+
+        self.reset()
+
         self.current_index -= 1
         try:
-            self.show_image(self.file_list[self.current_index])
+            file = self.file_list[self.current_index]
+            self.csv_content = [line for line in self.csv_content if line[0] != file]
+            self.show_image(file)
         except Exception as e:
             print(e)
 
     # function that is triggering rendering of the FFT
     def show_image(self, file):
-        os.system("renderfall -n 512 -v -f float32 -l 256 -w hann {}".format(file))
         png = file + ".png"
+        if not exists(png):
+            os.system("renderfall -n 512 -v -f float32 -l 256 -w hann {}".format(file))
+        
         self.data = imageio.imread(png)
         self.img.setImage(self.data)
         self.p1.setTitle(png)
+        self.current_filename = file
 
 
 
@@ -139,15 +206,14 @@ def main():
 
     cliParser = argparse.ArgumentParser(description='Plots quadrature IQ signals')    
     
-    cliParser.add_argument('input_dir', type=str, help='input directory containing the raw .cf32 files')
     cliParser.add_argument('output_csv', type=str, help='output csv file containing raw filenames and labelled data')
     args = cliParser.parse_args()
 
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
 
-    os.chdir(args.input_dir)
-    main.set_file_list(sorted(glob.glob("*.cf32"), key=os.path.getmtime))
+    main.set_file_list(sorted(glob.glob("/input/*.cf32"), key=os.path.getmtime))
+    main.set_output_file(args.output_csv)
     main.show()
 
     sys.exit(app.exec_())
