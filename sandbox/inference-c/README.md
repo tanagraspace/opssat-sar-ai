c09bc39746ef18723fbaf8c65bfd151677a38bd1
Georges Labrèche <georges@tanagraspace.com>
2022-08-04 00:16:13 -0600

    fixed some issues, still in development

--- sandbox/inference-c/README.md
+++ sandbox/inference-c/README.md
@@ -1,12 +1,32 @@
 # TF Inference
 Use the TensorFlow Lite C API to run oject detection inferences on SDR JPG spectra.
 - Uses the [stb library](https://github.com/georgeslabreche/stb) for image processing.
+- Uses the [TensorFlow GitHub Repo (branch r2.9)](https://github.com/tensorflow/tensorflow/tree/r2.9) as the compiler's include path option.
 
 ## Build
 - Initialize and update the stb Git submodule: `git submodule init && git submodule update`.
 - Compile with `make`. Can also compile for ARM architecture with `make TARGET=arm`.
 
 ## Run
+Super easy to run.
+
+### Helps
 ```bash
-./inference -i test/test_input.cf32.jpg -m test/97708c55_efficientdet_lite1.tflite -x 384 -y 384
+$ ./inference -?
+inference [options] ...
+  --input    / -i        input image filename
+  --model    / -m        tflite model filename
+  --xsize    / -x        training input width
+  --ysize    / -y        training input height
+  --mean     / -n        input mean (optional)
+  --std      / -s        input standard deviation (optional)
+  --help     / -?        this information
 ```
+
+### Inference
+```bash
+$ ./inference -i test/test_input.cf32.jpg -m test/97708c55_efficientdet_lite1.tflite -x 384 -y 384
+```
+
+## Note
+If the input mean is set 0 and the input standard deviation is set to 255 then the image input's 0-255 RGB range is rescaled/normalized to 0-1. Using normalized inputs is preferred with respect to performance with tensor calculations. Only normalize inputs if the model was trained with normalized training data.
--- sandbox/inference-c/src/main.c
+++ sandbox/inference-c/src/main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+/* tensorflow header summary: */
+/* https://github.com/tensorflow/tensorflow/blob/b5ee46383fe070c6c678a4fbacca63bfea202aa1/tensorflow/lite/c/README.md#header-summary= */
+#include "tensorflow/lite/c/c_api.h"
+#include "tensorflow/lite/c/c_api_experimental.h"
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/c/builtin_op_data.h"
+
+
 /* define convenience macros */
 #define streq(s1,s2)    (!strcmp ((s1), (s2)))
 
@@ -11,38 +19,39 @@
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize.h"
 
-/* todo: eventually remove */
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define IMAGE_CHANNELS                    STBI_grey
+#define IMAGE_CHANNELS        STBI_rgb
 
 
 typedef enum {
-    TF_ALLOCATE_TENSOR              = 10, // Error allocating tensors.
-    TF_RESIZE_TENSOR                = 11, // Error resizing tensor.
-    TF_ALLOCATE_TENSOR_AFTER_RESIZE = 12, // Error allocating tensors after resize.
-    TF_COPY_BUFFER_TO_INPUT         = 13, // Error copying input from buffer.
-    TF_INVOKE_INTERPRETER           = 14, // Error invoking interpreter.
-    TF_COPY_OUTOUT_TO_BUFFER        = 15, // Error copying output to buffer.
+    TF_ALLOCATE_TENSOR              = 10, /* error allocating tensors */
+    TF_RESIZE_TENSOR                = 11, /* error resizing tensor */
+    TF_ALLOCATE_TENSOR_AFTER_RESIZE = 12, /* error allocating tensors after resize */
+    TF_COPY_BUFFER_TO_INPUT         = 13, /* error copying input from buffer */
+    TF_INVOKE_INTERPRETER           = 14, /* error invoking interpreter */
+    TF_COPY_OUTOUT_TO_BUFFER        = 15, /* error copying output to buffer */
 } errorCode;
 
 
-// Dispose of the model and interpreter objects.
+/* dispose of the model and interpreter objects */
 int disposeTfLiteObjects(TfLiteModel* pModel, TfLiteInterpreter* pInterpreter)
 {
     if(pModel != NULL)
     {
-      TfLiteModelDelete(pModel);
+        TfLiteModelDelete(pModel);
     }
 
     if(pInterpreter)
     {
-      TfLiteInterpreterDelete(pInterpreter);
+        TfLiteInterpreterDelete(pInterpreter);
     }
 }
 
-int infer(uint8_t *img_buffer, char *model_filename, int input_xsize, int input_ysize)
+
+/* inference/prediction function */
+int infer(uint8_t *img_buffer, char *model_filename, int input_xsize, int input_ysize, int input_mean, int input_std)
 {
     /* the image size */
     int image_size = input_xsize * input_ysize * IMAGE_CHANNELS;
@@ -56,14 +65,15 @@ int infer(uint8_t *img_buffer, char *model_filename, int input_xsize, int input_
     /* create the interpreter */
     TfLiteInterpreter *interpreter = TfLiteInterpreterCreate(model, NULL);
 
-    /* Allocate tensors */
+    /* allocate tensors */
     tfl_status = TfLiteInterpreterAllocateTensors(interpreter);
 
-    /* Log and exit in case of error */
+    /* log and exit in case of error */
     if(tfl_status != kTfLiteOk)
     {
-      disposeTfLiteObjects(model, interpreter);
-      return TF_ALLOCATE_TENSOR;
+        disposeTfLiteObjects(model, interpreter);
+        printf("ERROR: %d\n", TF_ALLOCATE_TENSOR);
+        return TF_ALLOCATE_TENSOR;
     }
 
     int input_dims[4] = {1, input_ysize, input_xsize, IMAGE_CHANNELS};
@@ -72,8 +82,9 @@ int infer(uint8_t *img_buffer, char *model_filename, int input_xsize, int input_
     /* log and exit in case of error */
     if(tfl_status != kTfLiteOk)
     {
-      disposeTfLiteObjects(model, interpreter);
-      return TF_RESIZE_TENSOR;
+        disposeTfLiteObjects(model, interpreter);
+        printf("ERROR: %d\n", TF_RESIZE_TENSOR);
+        return TF_RESIZE_TENSOR;
     }
 
     tfl_status = TfLiteInterpreterAllocateTensors(interpreter);
@@ -81,21 +92,34 @@ int infer(uint8_t *img_buffer, char *model_filename, int input_xsize, int input_
     /* log and exit in case of error */
     if(tfl_status != kTfLiteOk)
     {
-      disposeTfLiteObjects(model, interpreter);
-      return TF_ALLOCATE_TENSOR_AFTER_RESIZE;
+        disposeTfLiteObjects(model, interpreter);
+        printf("ERROR: %d\n", TF_ALLOCATE_TENSOR_AFTER_RESIZE);
+        return TF_ALLOCATE_TENSOR_AFTER_RESIZE;
     }
 
-    /* The input tensor */
+    /* the input tensor */
     TfLiteTensor* input_tensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
 
-    /* Copy the JPG image data into into the input tensor */
-    tfl_status = TfLiteTensorCopyFromBuffer(input_tensor, img_buffer, image_size * sizeof(uint8_t));
+    /* the array that will collect the JPEG's RGB values */
+    /* the TfLiteTensorCopyFromBuffer function expect buffer of type float */
+    float img_buffer_rescaled[image_size];
+
+    /* the RGB range is 0-255. Rescale it based on given input mean and standard deviation */
+    /* e.g. with input mean 0 and input std 255 the 0-255 RGB range is rescaled to 0-1 */
+    for(int i = 0; i < image_size; i++)
+    {
+        img_buffer_rescaled[i] = ((float)img_buffer[i] - input_mean) / input_std;
+    }
+
+    /* copy the JPG image data into into the input tensor */
+    tfl_status = TfLiteTensorCopyFromBuffer(input_tensor, img_buffer_rescaled, image_size * sizeof(float));
     
     /* log and exit in case of error */
     if(tfl_status != kTfLiteOk)
     {
-      disposeTfLiteObjects(model, interpreter);
-      return TF_COPY_BUFFER_TO_INPUT;
+        disposeTfLiteObjects(model, interpreter);
+        printf("ERROR: %d\n", TF_COPY_BUFFER_TO_INPUT);
+        return TF_COPY_BUFFER_TO_INPUT;
     }
 
     /* invoke interpreter */
@@ -104,23 +128,39 @@ int infer(uint8_t *img_buffer, char *model_filename, int input_xsize, int input_
     /* log and exit in case of error */
     if(tfl_status != kTfLiteOk)
     {
-      disposeTfLiteObjects(model, interpreter);
-      return TF_INVOKE_INTERPRETER;
+        disposeTfLiteObjects(model, interpreter);
+        printf("ERROR: %d\n", TF_INVOKE_INTERPRETER);
+        return TF_INVOKE_INTERPRETER;
     }
 
     /* extract the output tensor data */
-    const TfLiteTensor* output_tensor_boxes = TfLiteInterpreterGetOutputTensor(interpreter, 0);
-    const TfLiteTensor* output_tensor_classes = TfLiteInterpreterGetOutputTensor(interpreter, 1);
-    const TfLiteTensor* output_tensor_score = TfLiteInterpreterGetOutputTensor(interpreter, 2);
-    const TfLiteTensor* output_tensor_count = TfLiteInterpreterGetOutputTensor(interpreter, 3);
+
+    /* bounding boxes */
+    const TfLiteTensor *output_tensor_boxes = TfLiteInterpreterGetOutputTensor(interpreter, 0);
+
+    /* labels (there is only 1 label...) */
+    const TfLiteTensor *output_tensor_classes = TfLiteInterpreterGetOutputTensor(interpreter, 1);
+
+    /* score per bounding box */
+    const TfLiteTensor *output_tensor_score = TfLiteInterpreterGetOutputTensor(interpreter, 2);
+
+    /* total bounding box count */
+    const TfLiteTensor *output_tensor_count = TfLiteInterpreterGetOutputTensor(interpreter, 3);  
+    float count[1];
+    TfLiteStatus status = TfLiteTensorCopyToBuffer(output_tensor_count, count, sizeof(float));
+    printf("\ncount: %f", count[0]);
+
 
     /* todo: do the doings */
 
     /* dispose of the TensorFlow objects */
     disposeTfLiteObjects(model, interpreter);
+
+    printf("6\n");
 }
 
 
+/* main function */
 int main(int argc, char *argv [])
 {
     /* return code to track general errors */
@@ -131,6 +171,8 @@ int main(int argc, char *argv [])
     int argv_index_model = -1;
     int argv_index_xsize = -1;
     int argv_index_ysize = -1;
+    int argv_index_mean  = -1;
+    int argv_index_std   = -1;
 
     // --------------------------------------------------------------------------
     // parse the command arguments (all arguments are optional)
@@ -146,6 +188,8 @@ int main(int argc, char *argv [])
             printf("\n  --model    / -m        tflite model filename");
             printf("\n  --xsize    / -x        training input width");
             printf("\n  --ysize    / -y        training input height");
+            printf("\n  --mean     / -n        input mean (optional)");
+            printf("\n  --std      / -s        input standard deviation (optional)");
             printf("\n  --help     / -?        this information\n\n");
             
             /* program exit code */
@@ -167,6 +211,14 @@ int main(int argc, char *argv [])
         if (streq (argv [argn], "--ysize")
         ||  streq (argv [argn], "-y"))
             argv_index_ysize = ++argn;
+        else
+        if (streq (argv [argn], "--mean")
+        ||  streq (argv [argn], "-n"))
+            argv_index_mean = ++argn;
+        else
+        if (streq (argv [argn], "--std")
+        ||  streq (argv [argn], "-s"))
+            argv_index_std = ++argn;
         else
         {
             /* print error message */
@@ -192,20 +244,24 @@ int main(int argc, char *argv [])
     int img_channels_infile;
 
     /* target properties for the resized image */
-    int img_xsize_training = atoi(argv[argv_index_xsize]);
-    int img_ysize_training = atoi(argv[argv_index_ysize]);
+    int img_xsize_model_input = atoi(argv[argv_index_xsize]);
+    int img_ysize_model_input = atoi(argv[argv_index_ysize]);
 
-    // get the model filename 
+    /* mean and standard deviation, use default values if not specified */
+    int input_mean = argv_index_mean < 0 ? 0 : atoi(argv[argv_index_mean]);
+    int input_std = argv_index_std < 0 ? 1 : atoi(argv[argv_index_std]);
+
+    /* get the model filename */ 
 
     /* decode the image file */
     uint8_t *img_buffer = (uint8_t*)stbi_load(argv[argv_index_input], &img_xsize, &img_ysize, &img_channels_infile, IMAGE_CHANNELS);
 
     /* initialize resized image buffer to the training input size */
-    int img_buffer_training_input_size = img_xsize_training * img_ysize_training * IMAGE_CHANNELS;
+    int img_buffer_training_input_size = img_xsize_model_input * img_ysize_model_input * IMAGE_CHANNELS;
     uint8_t img_buffer_resized[img_buffer_training_input_size];
 
     /* downsample the image i.e., resize the image to a smaller dimension */
-    rc = stbir_resize_uint8(img_buffer, img_xsize, img_ysize, 0, img_buffer_resized, img_xsize_training, img_ysize_training, 0, IMAGE_CHANNELS);
+    rc = stbir_resize_uint8(img_buffer, img_xsize, img_ysize, 0, img_buffer_resized, img_xsize_model_input, img_ysize_model_input, 0, IMAGE_CHANNELS);
 
     /* returned result is 1 for success or 0 in case of an error */
     if(rc == 0)
@@ -221,7 +277,7 @@ int main(int argc, char *argv [])
         /* write image to file */
         /* todo: eventually remove */
         int img_quality_desired = 90;
-        rc = stbi_write_jpg("test/training_input.jpg", img_xsize_training, img_ysize_training, IMAGE_CHANNELS, (void *)img_buffer_resized, img_quality_desired);
+        rc = stbi_write_jpg("test/training_input.jpg", img_xsize_model_input, img_ysize_model_input, IMAGE_CHANNELS, (void *)img_buffer_resized, img_quality_desired);
 
         /* the stb_write functions returns 0 on failure and non-0 on success. */
         if(rc == 0)
@@ -232,14 +288,14 @@ int main(int argc, char *argv [])
         else
         {
             /* success message */
-            printf("Resized: %d x %d --> %d x %d\n", img_xsize, img_ysize, img_xsize_training, img_ysize_training);
+            printf("Resized: %d x %d --> %d x %d\n", img_xsize, img_ysize, img_xsize_model_input, img_ysize_model_input);
         } 
     }
 #endif
 
 
     /* inference */
-    infer(img_buffer, argv[argv_index_model], img_xsize_training, img_ysize_training);
+    infer(img_buffer_resized, argv[argv_index_model], img_xsize_model_input, img_ysize_model_input, input_mean, input_std);
     
     /* free the input image data buffer */
     stbi_image_free(img_buffer);
